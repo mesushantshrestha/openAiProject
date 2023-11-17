18fd8f8899952c55f836c16a35823c227e0854ea
KILLERPLAY\kille <mesushantshrestha@gmail.com>
2023-11-17 14:22:09 -0500

    initial phase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MathTutorComponent} from "./math-tutor/math-tutor.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MathTutorComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'openAiProject';
+  constructor() {
+  }
 }
